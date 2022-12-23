998e93e44963a5e94748ccd90e7709a47ee32110
Harald Meyer <hame@softwareag.com>
2022-12-23 14:20:04 +0100

    verify hub call

--- ax-mm-hubconnector/test/main/java/com/adamos/hubconnector/services/EventRulesServiceTest.java
+++ ax-mm-hubconnector/test/main/java/com/adamos/hubconnector/services/EventRulesServiceTest.java
@@ -3,6 +3,8 @@ package com.adamos.hubconnector.services;
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
 
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.util.Collections;
 
 import org.joda.time.DateTime;
@@ -12,9 +14,12 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpMethod;
+import org.springframework.web.client.RestClientException;
 
 import com.adamos.hubconnector.CustomProperties;
 import com.adamos.hubconnector.model.HubConnectorGlobalSettings;
+import com.adamos.hubconnector.model.events.AdamosEventData;
 import com.adamos.hubconnector.model.events.EventMapping;
 import com.adamos.hubconnector.model.hub.EquipmentDTO;
 import com.cumulocity.model.idtype.GId;
@@ -51,13 +56,15 @@ class EventRulesServiceTest {
 	}
 
 	@Test
-	void testMapEventsOneDeviceOneEvent() {
+	void testMapEventsOneDeviceOneEvent() throws RestClientException, URISyntaxException {
 		EventMapping mapping = new EventMapping();
 		mapping.setId("m123");
 		mapping.setC8yEventType("cet123");
 		mapping.setAdamosEventType("aet123");
 		mapping.setC8yFragments(Collections.emptyList());
 		mapping.setEnabled(true);
+		HubConnectorGlobalSettings settings = new HubConnectorGlobalSettings(true);
+		settings.setEnvironment("adamos-hub.dev");
 		ManagedObjectRepresentation device = device("d123");
 		when(
 				cumulocityService.getEvents(eq(device), eq(mapping.getC8yEventType()), any())
@@ -67,9 +74,21 @@ class EventRulesServiceTest {
 		when(
 				hubConnectorService.getGlobalSettings()
 		).thenReturn(
-				new HubConnectorGlobalSettings(true)
+				settings
 		);
 		ers.mapEvents(mapping, Collections.singletonList(device));
+		verify(hubService).restToHub(
+				eq(new URI("https://services.adamos-hub.dev/event-service/v1/event")),
+				eq(HttpMethod.POST),
+				argThat(x -> {
+					AdamosEventData data = (AdamosEventData) x;
+					assertEquals(mapping.getAdamosEventType(), data.getEventCode());
+					assertEquals("adamos:masterdata:type:machine:1", data.getReferenceObjectType());
+					assertEquals("uuid123", data.getReferenceObjectId());
+					return true;
+					}),
+				eq(AdamosEventData.class)
+				);
 	}
 
 	private EventRepresentation event(String type, ManagedObjectRepresentation source) {
